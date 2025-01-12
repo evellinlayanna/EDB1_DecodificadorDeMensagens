466f3b3806847608fa47c45517b78f48b2948fd5
Evellin Layanna <72310367+evellinlayanna@users.noreply.github.com>
2025-01-12 18:05:31 -0300

    Adição de comentários

--- hash.c
+++ hash.c
@@ -4,22 +4,22 @@
 
 void inicializarHash(Hash *h) {
   for (int i = 0; i < MAX_HASH; i++) {
-    h->frequencias[i] = 0;
-    h->ocupado[i] = 0;
+    h->frequencias[i] = 0; // Inicia a frequência dos caracteres com zero
+    h->ocupado[i] = 0; // Marca os índices como não ocupados
   }
 }
 
 void calcularFrequencias(Hash *h, const char *mensagem) {
   for (int i = 0; mensagem[i] != '\0'; i++) {
     int index = mensagem[i] - 'a';
-    h->frequencias[index]++;
+    h->frequencias[index]++; // Incrementa a frequência desse caractere
     h->ocupado[index] = 1;
   }
 }
 
 int obterFrequencia(Hash *h, char caractere) {
   int index = caractere - 'a';
-  return h->ocupado[index] ? h->frequencias[index] : 0;
+  return h->ocupado[index] ? h->frequencias[index] : 0; // Retorna a frequência, ou zero se o caractere não for encontrado
 }
 
 void limparHash(Hash *h) { inicializarHash(h); }
